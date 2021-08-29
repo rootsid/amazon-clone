4ead50e637403a2acbaf4925dabc0bdf6a123ab9
sidharth kaushik <sidharth.kaushik@atos.net>
2021-08-29 12:58:33 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello</h1>
+      {/* Header */}
+        
+      {/* Home */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCGZEZtnsm0GQXj488ETOx9Q-qCfK53goA",
+    authDomain: "clone-62adf.firebaseapp.com",
+    projectId: "clone-62adf",
+    storageBucket: "clone-62adf.appspot.com",
+    messagingSenderId: "156353551141",
+    appId: "1:156353551141:web:b26687110e151098a298cc",
+    measurementId: "G-YEZ3R7SXJ1"
+  };
